ae7c7ccf02f987097ac0458023afa34227998b19
Gbemisola Odukoya <folaryz@gmail.com>
2022-08-14 17:02:46 +0100

    Created Navbar

---
+++ src/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import AirbnbLogo from './images/airbnb 1.png'
+
+const Navbar = () => {
+  return (
+<div className='navbar' >
+    <img src={AirbnbLogo} alt="" />
+</div>
+  )
+}
+
+export default Navbar
